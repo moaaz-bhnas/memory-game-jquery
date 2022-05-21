a63f612463935e6dddea31d6b27d6e3a04f821e4
moaaz-bhnas <moaaz_bs@yahoo.com>
2022-05-21 16:07:39 +0200

    first commit

--- js/index.js
+++ js/index.js
@@ -651,17 +651,3 @@ const modalView = {
 };
 
 octopus.init();
-
-// Register Service Worker
-if ("serviceWorker" in navigator) {
-  window.onload = function () {
-    navigator.serviceWorker
-      .register("./sw.js")
-      .then((registration) => {
-        console.log("SW registered: ", registration);
-      })
-      .catch((registrationError) => {
-        console.log("SW registration failed: ", registrationError);
-      });
-  };
-}
